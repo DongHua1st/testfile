26e0d5cda7eec6c91078e07edc4075d9f6fd8fe3
ChenWei <1862156805@163.com>
2023-07-02 11:37:33 +0800

    create

---
+++ customerinfo.sql
@@ -0,0 +1,95 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : DB
+ Source Server Type    : MySQL
+ Source Server Version : 80032 (8.0.32)
+ Source Host           : localhost:3306
+ Source Schema         : customerinfo
+
+ Target Server Type    : MySQL
+ Target Server Version : 80032 (8.0.32)
+ File Encoding         : 65001
+
+ Date: 29/06/2023 15:18:36
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for activity
+-- ----------------------------
+DROP TABLE IF EXISTS `activity`;
+CREATE TABLE `activity`  (
+  `ActivityID` int NOT NULL,
+  `ActivityContent` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `ActivityType` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `ActivityTheme` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `ActivityCategory` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `EmployeeID` int NULL DEFAULT NULL,
+  `DepartmentID` int NULL DEFAULT NULL,
+  PRIMARY KEY (`ActivityID`) USING BTREE,
+  INDEX `FK_Activity_Employee`(`EmployeeID` ASC) USING BTREE,
+  INDEX `FK_Activity_Department`(`DepartmentID` ASC) USING BTREE,
+  CONSTRAINT `FK_Activity_Department` FOREIGN KEY (`DepartmentID`) REFERENCES `department` (`DepartmentID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `FK_Activity_Employee` FOREIGN KEY (`EmployeeID`) REFERENCES `employee` (`EmployeeID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for customer
+-- ----------------------------
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer`  (
+  `CustomerID` int NOT NULL,
+  `CustomerName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `CustomerCategory` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `CustomerContact` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `CustomerAddress` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `CustomerNotes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  PRIMARY KEY (`CustomerID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for customerservice
+-- ----------------------------
+DROP TABLE IF EXISTS `customerservice`;
+CREATE TABLE `customerservice`  (
+  `ServiceID` int NOT NULL,
+  `ServiceContent` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `ServiceType` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `HandlingLevel` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `Feedback` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  `Satisfaction` int NULL DEFAULT NULL,
+  `CustomerID` int NULL DEFAULT NULL,
+  `ServiceStatus` int NULL DEFAULT NULL,
+  PRIMARY KEY (`ServiceID`) USING BTREE,
+  INDEX `CustomerID`(`CustomerID` ASC) USING BTREE,
+  CONSTRAINT `customerservice_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for department
+-- ----------------------------
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department`  (
+  `DepartmentID` int NOT NULL,
+  `DepartmentName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`DepartmentID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for employee
+-- ----------------------------
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE `employee`  (
+  `EmployeeID` int NOT NULL,
+  `DepartmentID` int NULL DEFAULT NULL,
+  `EmployeeName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `EmployeePosition` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`EmployeeID`) USING BTREE,
+  INDEX `DepartmentID`(`DepartmentID` ASC) USING BTREE,
+  CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`DepartmentID`) REFERENCES `department` (`DepartmentID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
